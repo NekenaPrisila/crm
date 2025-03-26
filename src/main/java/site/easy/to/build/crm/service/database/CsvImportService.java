26c2eafcfec26b924edfc94929790cbef0da5a5f
nekena123 <rakotomanjatonekena@gmail.com>
2025-03-26 06:29:23 +0300

    Update CsvImportService.java

--- src/main/java/site/easy/to/build/crm/service/database/CsvImportService.java
+++ src/main/java/site/easy/to/build/crm/service/database/CsvImportService.java
@@ -204,10 +204,6 @@ public class CsvImportService {
 
         List<TicketLeadCsvDto> dtos = csvToBean.parse();
 
-        for (TicketLeadCsvDto ticketLeadCsvDto : dtos) {
-            System.out.println("valeurr" + ticketLeadCsvDto.getExpense());
-        }
-
         for (int i = 0; i < dtos.size(); i++) {
             int lineNumber = i + 2;
             TicketLeadCsvDto dto = dtos.get(i);
